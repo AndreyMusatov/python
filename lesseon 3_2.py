d613c810a6569e4f53fe4b40689fa3f93f32c2d6
Andrey <76228053+AndreyMusatov@users.noreply.github.com>
2021-01-19 12:26:53 +0300

    Delete lesseon 3_2.py

--- lesseon 3_2.py
+++
@@ -1,9 +0,0 @@
-#Задача 2
-def database(surname, name, year, city, email, telephone):
-    return ' '.join([surname, name,  year, city, email, telephone])
-
-
-
-print(database (surname = input("введите фамилию :"), name = input('введите имя :'), year = input("введите год рождения :"), city = input("введите ваш город проживания :"),
-                email= input("введите эл.почту :"), telephone = input("Введите телефон :")))
-
